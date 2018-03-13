f547cf4d4d428331b4dfe2a774b00863b5c58173
chuancw <2402873932@qq.com>
2018-03-13 23:47:05 +0800

    learn lxf python3 base1

---
+++ learn_lxf_python3/base.py
@@ -0,0 +1,35 @@
+print('I\'m ok')
+print("I'm ok")
+
+print('I\'m learning\nPython3')
+
+print('\\\t\\')
+
+print(r'\\\t\\')
+
+print('''line1
+line2
+line3''')
+
+print(True)
+print(4<1)
+
+age = 32
+if age >= 18:
+    print('adult')
+else:
+    print('teen')
+
+print(None)
+adu = None
+print(adu)
+
+a = 'abc'
+b = a
+a = 'xyz'
+print(b)
+
+print(10/3)
+print(10//3)
+print(10 % 3)
+
