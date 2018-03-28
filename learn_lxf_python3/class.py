c172de003f89b13d9a71adf13baa613f5139c438
chuancw <2402873932@qq.com>
2018-03-29 00:08:46 +0800

    python3 learn base

--- learn_lxf_python3/class.py
+++ learn_lxf_python3/class.py
@@ -5,6 +5,9 @@ class student(object):
         self.name = name
         self.score = score
 
+    # 在类中定义的函数只有一点不同
+    # 就是第一个参数永远是实例变量self
+    # 并且，调用时，不用传递该参数
     def print_attr(self):
         print('%s, %s' % (self.name, self.score))
 
@@ -12,6 +15,6 @@ class student(object):
 Tom = student('Tom', 100)
 Tom.print_attr()
 
-
+# 可以自由地给一个实例变量绑定属性
 Tom.age = 21
 print(Tom.age)
